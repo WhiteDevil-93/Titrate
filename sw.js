dcc8178eb42e421ff73e8ae179ae1fdaea876011
Tashriq Hendricks <tashriq93@gmail.com>
2026-07-16 13:41:38 +0200

    v4.6 SW

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-_PLACEHOLDER_SW2
+// Service Worker v4.6 - Kill switch pattern
+const CACHE_NAME = 'titrate-v46';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))
+    .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+});
