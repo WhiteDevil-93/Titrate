f65c1c4ff37e894a70ce738e8f4d55f4941f2aff
Tashriq Hendricks <tashriq93@gmail.com>
2026-07-16 13:42:28 +0200

    v4.6 service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
-_PLACEHOLDER_SW1
+// Service Worker - Cache busting kill-switch
+const CACHE_NAME = 'titrate-v46';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))
+    .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+});
